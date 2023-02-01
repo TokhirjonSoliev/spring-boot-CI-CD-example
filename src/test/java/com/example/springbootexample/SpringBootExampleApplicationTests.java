6c374b3c7bc538c8cbd9735c19e8499855f3c41b
tokhirjon <tokhirjon007@gmail.com>
2023-02-01 11:05:17 +0500

    update Jenkinsfile

--- src/test/java/com/example/springbootexample/SpringBootExampleApplicationTests.java
+++ src/test/java/com/example/springbootexample/SpringBootExampleApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.springbootexample;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class SpringBootExampleApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
